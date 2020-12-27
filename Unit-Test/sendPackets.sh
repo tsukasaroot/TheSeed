962a7243e8632f365fcf764df3f4749f25b21f99
Jordan <jordan.rouco-miguez@epitech.eu>
2020-12-27 17:12:35 +0100

    Update sendPackets.sh

--- Unit-Test/sendPackets.sh
+++ Unit-Test/sendPackets.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
-echo -e "login:test:puissant0x12" | nc -u 51.210.41.122 11102
-n=1
-while [ $n -le 50 ]
-do
-	echo -e "getClientData:test0x12" | nc -u 51.210.41.122 11102
-done
-echo -e "logout:test0x12" | nc -u 51.210.41.122 11102
+echo -e "login:test:puissant0x12" | nc -u -w 1 51.210.41.122 11102
+
+echo -e "getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12" | nc -u -w 1 51.210.41.122 11102
+echo -e "getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12" | nc -u -w 1 51.210.41.122 11102
+echo -e "getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12" | nc -u -w 1 51.210.41.122 11102
+echo -e "getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12" | nc -u -w 1 51.210.41.122 11102
+echo -e "getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12" | nc -u -w 1 51.210.41.122 11102
+echo -e "getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12" | nc -u -w 1 51.210.41.122 11102
+echo -e "getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12" | nc -u -w 1 51.210.41.122 11102
+echo -e "getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12" | nc -u -w 1 51.210.41.122 11102
+echo -e "getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12" | nc -u -w 1 51.210.41.122 11102
+echo -e "getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12" | nc -u -w 1 51.210.41.122 11102
+echo -e "getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12" | nc -u -w 1 51.210.41.122 11102
+echo -e "getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12" | nc -u -w 1 51.210.41.122 11102
+echo -e "getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12" | nc -u -w 1 51.210.41.122 11102
+echo -e "getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12" | nc -u -w 1 51.210.41.122 11102
+echo -e "getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12" | nc -u -w 1 51.210.41.122 11102
+echo -e "getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12getClientData:test0x12" | nc -u -w 1 51.210.41.122 11102
+
+echo -e "logout:test0x12" | nc -u -w 1 51.210.41.122 11102
