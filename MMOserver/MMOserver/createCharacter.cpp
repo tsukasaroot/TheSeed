16dbd7df4a4d93d3b835e65b866e7bd0fef5e19d
Jordan <jordan.rouco-miguez@epitech.eu>
2022-07-30 19:17:37 +0200

    Update createCharacter.cpp

--- MMOserver/MMOserver/createCharacter.cpp
+++ MMOserver/MMOserver/createCharacter.cpp
@@ -6,6 +6,7 @@
 
 void Server::initCreateCharacters(std::vector<std::string> cmd)
 {
+	// probably all of the below should be loaded at start from datasheets tehe
 	// retrieve all disponible class
 	// retrieve all disponible races
 	// retrieve all id's for every part of bodies possible
